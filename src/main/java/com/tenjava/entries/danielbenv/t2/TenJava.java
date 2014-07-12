363083332e879bf6018a52074b0a9abbc3f26d0f
Daniel Benvenisti <danielben60@gmail.com>
2014-07-12 19:28:30 +0300

    I stop doing it because I don't have an idea and I had to start very late. I'm sorry, I really want to participate but I can't. This is my last commit.

--- src/main/java/com/tenjava/entries/danielbenv/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/danielbenv/t2/TenJava.java
@@ -2,6 +2,8 @@ package com.tenjava.entries.danielbenv.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.tenjava.entries.danielbenv.t2.executors.CommandExecutor;
+
 public class TenJava extends JavaPlugin {
 	
 	// The main class instance
@@ -19,6 +21,7 @@ public class TenJava extends JavaPlugin {
 	public void onEnable() {
 		// Setting the instance to this
 		instance = this;
+		getCommand("energy").setExecutor(new CommandExecutor(this));
 	}
 	
 	public void onDisable() {
--- src/main/java/com/tenjava/entries/danielbenv/t2/executors/CommandExecutor.java
+++ src/main/java/com/tenjava/entries/danielbenv/t2/executors/CommandExecutor.java
@@ -1,12 +1,35 @@
 package com.tenjava.entries.danielbenv.t2.executors;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.tenjava.entries.danielbenv.t2.TenJava;
 
 public class CommandExecutor implements org.bukkit.command.CommandExecutor {
 
+	private TenJava instance;
+	private String[] usage = { ChatColor.RED + "A list of commands: ", "" };
+	
+	public CommandExecutor(TenJava plugin) {
+		this.instance = plugin;
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		// Player check
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("Players only");
+			return false;
+		}
+		// Checking if the command is equals (ignore case) to energy
+		if(cmd.getName().equalsIgnoreCase("energy")) {
+			if(args.length == 0) {
+				
+				return false;	
+			}
+		}
 		return false;
 	}
 
